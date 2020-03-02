80d55de5acac578ae1e38f617428f070a347ee34
Uchio Kondo <udzura@udzura.jp>
2020-03-02 17:39:46 +0900

    Pass build for Mac...

--- ext/usdt_marker/usdt_marker.c
+++ ext/usdt_marker/usdt_marker.c
@@ -3,6 +3,7 @@
 
 VALUE rb_mUsdtMarker;
 
+#ifndef __APPLE__
 static VALUE
 rb_probe_i2 (VALUE mod, VALUE arg0, VALUE arg1) {
   DTRACE_PROBE2(rubygem, usdt_marker_i2, NUM2LONG(arg0), NUM2LONG(arg1));
@@ -14,6 +15,20 @@ rb_probe_i1s1 (VALUE mod, VALUE arg0, VALUE arg1) {
   DTRACE_PROBE2(rubygem, usdt_marker_i1s1, NUM2LONG(arg0), StringValuePtr(arg1));
   return Qtrue;
 }
+#else
+#include <err.h>
+static VALUE
+rb_probe_i2 (VALUE mod, VALUE arg0, VALUE arg1) {
+  warn("MacOS is never supported, because they would not have DTRACE_PROBE*\n");
+  return Qtrue;
+}
+
+static VALUE
+rb_probe_i1s1 (VALUE mod, VALUE arg0, VALUE arg1) {
+  warn("MacOS is never supported, because they would not have DTRACE_PROBE*\n");
+  return Qtrue;
+}
+#endif
 
 void
 Init_usdt_marker(void)
